1c948fd5925e36a7d7632bf09df5d48b988c11b1
jamie2210 <jamieletts2210@gmail.com>
2023-03-04 16:02:09 +0000

    Feature: Add alphabet buttons

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,13 +1,28 @@
 let batmanWord = [
     'joker', 'catwoman', 'robin', 'the riddler', 'bruce wayne', 'gotham city', 'justice league', 'batmobile', 'talia al ghul', 'alfred pennyworth', 'james gordon', 'dick grayson',
-    'nightwing', 'damian wayne', 'wonder woman', 'wayne manor', 'harvey two face', 'arkum asylum', 'the penguin', 'wayne tower', 'the dark knight']
+    'nightwing', 'damian wayne', 'wonder woman', 'wayne manor', 'harvey two face', 'arkum asylum', 'the penguin', 'wayne tower', 'the dark knight', 'poison ivy', 'dr freeze', 'scarecrow']
 
 let answer = '';
 
-function picWord () {
+function selectWord () {
     answer = batmanWord[Math.floor(Math.random() * batmanWord.length)];
-
-    alert(answer);
 }
 
-picWord();
+selectWord();
+
+function keyboard() {
+    buttons = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+        `
+        <button
+          class="button-keys"
+          id='` + letter + `
+          onclick="checkGuess('` + letter + `')"
+          >
+          ` + letter + `
+        </button>  
+        `
+        ).join('');
+    document.getElementById('keys').innerHTML = buttons;
+}
+
+keyboard();
