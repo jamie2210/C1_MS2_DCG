2c56ab96e7c03072f8d9f411e0ef3582792be1fd
jamie2210 <jamieletts2210@gmail.com>
2023-03-04 17:39:57 +0000

    Feature: Add hidden word and gallows image

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -3,6 +3,10 @@ let batmanWord = [
     'nightwing', 'damian wayne', 'wonder woman', 'wayne manor', 'harvey two face', 'arkum asylum', 'the penguin', 'wayne tower', 'the dark knight', 'poison ivy', 'dr freeze', 'scarecrow']
 
 let answer = '';
+let maxGuesses = 7;
+let mistakes = 0;
+let guessed = [];
+let word = [];
 
 function selectWord () {
     answer = batmanWord[Math.floor(Math.random() * batmanWord.length)];
@@ -12,17 +16,20 @@ selectWord();
 
 function keyboard() {
     buttons = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
-        `
-        <button
-          class="button-keys"
-          id='` + letter + `
-          onclick="checkGuess('` + letter + `')"
-          >
-          ` + letter + `
-        </button>  
-        `
+        `<button class="button-keys" id='` + letter + `onclick="checkGuess('` + letter + `')">` + letter + `</button> `
         ).join('');
     document.getElementById('keys').innerHTML = buttons;
 }
 
 keyboard();
+
+function hiddenWord() {
+    word = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('guessword').innerHTML = word;
+}
+
+hiddenWord();
+
+document.getElementById('maxguesses').innerHTML = maxGuesses;
+
