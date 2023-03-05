80cfe308e6f4562d58dd961424bc87b7951f0e48
jamie2210 <jamieletts2210@gmail.com>
2023-03-05 18:44:09 +0000

    Feature: Add mistakes count and hangman image functions

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,25 +1,26 @@
-let batmanWord = [
+let batmanPhrase = [
     'Joker', 'Catwoman', 'Robin', 'The Riddler', 'Bruce Wayne', 'Gotham City', 'Justice League', 'Batmobile', 'Talia Al Ghul', 'Alfred Pennyworth', 'James Gordon', 'Dick Grayson', 'Harley Quinn',
     'Nightwing', 'Damian Wayne', 'Wonder Woman', 'Wayne Manor', 'Harvey Two Face', 'Arkum Asylum', 'The Penguin', 'Wayne Tower', 'The Dark Knight', 'Poison Ivy', 'Dr Freeze', 'Scarecrow']
 
 let answer = '';
-let maxGuesses = 7;
-let mistakes = 0;
+let maxGuesses = 6;
+let wrongGuess = 0;
 let wins = 0;
 let losses = 0;
 let guessed = [];
 let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let word = '';
+
+document.getElementById('maxguesses').innerHTML = maxGuesses;
 
 /**
  * Select randon word for game
  */
 
-function selectWord () {
-    answer = batmanWord[Math.floor(Math.random() * batmanWord.length)];
+function selectPhrase () {
+    answer = batmanPhrase[Math.floor(Math.random() * batmanPhrase.length)];
 }
 
-selectWord();
+selectPhrase();
 
 /**
  * Populate keyboard buttons in the game screen
@@ -28,43 +29,48 @@ function createKeyboard() {
     let keyboard = "";
     for(let i = 0; i < alphabet.length; i++) {
         let letter = alphabet[i];
-        keyboard += `<button class="button-keys" id="btn-${letter}">${letter}</button>`;
+        keyboard += `<button class="button-keys" onClick="handleGuess('` + letter + `')" id="btn-${letter}">${letter}</button>`;
     }
     document.getElementById('keys').innerHTML = keyboard;
+    keyboardEventListeners();
 }
 
 createKeyboard();
 
 /**
- * Set up underscores for letters in the word and blank space between words
+ * Set up dashes for letters in the word and blank space between words
  * @param {string} answer
- * @returns {string} word
+ * @returns {string} dashes
  */
-function hiddenWord(answer) {
-    let word ='';
+function hiddenPhrase(answer) {
+    let dashes ='';
     for(let i = 0; i < answer.length; i++) {
         let letter = answer[i];
         if (letter !== ' ') {
-            word += '_';
+            dashes += '_';
         } else {
-            word += ' ';
+            dashes += ' ';
         }
     }
-    return word;
+    return dashes;
 }
 
 /**
- * Display underscores for hidden word
- * @param {string} phrase 
+ * Display dashes for hidden word
+ * @param {string} answer 
  */
-function setHiddenWord(answer) {
-    let guessingWord = hiddenWord(answer);
-    document.getElementById('guessword').innerHTML = guessingWord;
+function setHiddenPhrase(answer) {
+    let guessingPhrase = hiddenPhrase(answer);
+    document.getElementById('guessphrase').innerHTML = guessingPhrase;
     alert(answer);
 }
 
-setHiddenWord(answer);
+setHiddenPhrase(answer);
 
+/**
+ * Add event listeners to keyboard buttons to enable letter guessing with use of mouse and keyboard and add styling to match other game buttons
+ * Call checkLetter function to verify if the hidden phrase contains guessed letter
+ */
 function keyboardEventListeners() {
     document.getElementById('keys').addEventListener('click', function (event) {
         if (!event.target.className.includes('button-keys')) return;
@@ -99,7 +105,35 @@ function keyboardEventListeners() {
     });
 }
 
-keyboardEventListeners();
 
-document.getElementById('maxguesses').innerHTML = maxGuesses;
+function checkLetter(letter) {
+    if (answer.includes(letter.toLowerCase()) || answer.includes(letter)) {
+        addLetter(letter);
+    } else {
+        document.getElementById('wrongguess').innerHTML = wrongGuess += 1;
+    }
+    wrongGuessImage();
+    youLost();
+}
 
+function wrongGuessImage() {
+    if (wrongGuess === 1) {
+        document.getElementById('gallows').src = 'assets/images/joker2.png';
+      } else if (wrongGuess === 2) {
+        document.getElementById('gallows').src = 'assets/images/joker3.png';   
+      } else if (wrongGuess === 3) {
+        document.getElementById('gallows').src = 'assets/images/joker4.png';
+      } else if (wrongGuess === 4) {
+        document.getElementById('gallows').src = 'assets/images/joker5.png';
+      } else if (wrongGuess === 5) {
+        document.getElementById('gallows').src = 'assets/images/joker6.png';
+      } else if (wrongGuess === 6) {
+        document.getElementById('gallows').src = 'assets/images/joker7.png';
+      }
+}
+
+function youLost() {
+    if (wrongGuess === 6) {
+        document.getElementById('keys').innerHTML = `<h1>You Lost!</h1>`;
+    }
+}
