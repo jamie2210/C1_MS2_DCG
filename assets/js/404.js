df020d7f090437d2e4faeea4957e973c79d49248
jamie2210 <jamieletts2210@gmail.com>
2023-03-08 06:55:18 +0000

    Feature: Add count down timer to 404 page to automatically redirect back to index.html

---
+++ assets/js/404.js
@@ -0,0 +1,16 @@
+/**
+ * Count down timer set to 5 seconds before page is re-directed to index.html
+ */
+
+function countDownTimer() {
+    setInterval(function() {
+    let timer = document.getElementById('countdown');
+    let count = timer.textContent * 1-1;
+    timer.textContent = count;
+    if (count <= 0) {
+        window.location.replace("index.html")
+        }
+    }, 1000);
+}
+
+countDownTimer();
