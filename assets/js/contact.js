bc62badcef7dc61e755ef4d3231ed84b9259ce5f
jamie2210 <jamieletts2210@gmail.com>
2023-03-12 19:36:35 +0000

    Feature: Add styled alert box and form reset function and comments to code

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,8 @@
+/**
+ * Add email.js to contact form to send email directly to gmail account.
+ * @param {sendEmail} contactForm 
+ */
+
 function sendMail(contactForm) {
     emailjs.send("service_xm3vl8h","dcgame", {
         "from_name" : contactForm.name.value,
@@ -8,9 +13,14 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            swal("Thank you!", "Your message has been sent", "success"); // Use sweet alert to provide stlyed alert box for successfully sent message.
+            contactForm.reset(); // Reset form after successful submission.
         },
         function(error) {
             console.log("FAILED", error);
+            swal("Oh dear!", "Something went wrong, please try again", "error"); // Use sweet alert to provide stlyed alert box for unsuccessfully sent message.
         });
-        return false;
+        return false; // Prevents form from submitting if there's an error.
 }
+
+
