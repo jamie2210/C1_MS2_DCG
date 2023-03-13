27bf95d68c21c747e0616ec69308eb17dcb014c2
jamie2210 <jamieletts2210@gmail.com>
2023-03-13 08:09:26 +0000

    Feauture: Add javascript to modals

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
+let batmanModal = document.querySelector('.batman-modal') ;
 
+let riddlerModal = document.querySelector('.riddler-modal');
+
+let button = document.getElementsByClassName('close');
+
+function batmanRules() {
+    batmanModal.style.display = "block";
+
+}
+
+function riddlerRules() {
+    riddlerModal.style.display = "block";
+}
+
+function batmanClose() {
+    batmanModal.style.display = "none";
+}
+
+function riddlerClose() {
+    riddlerModal.style.display = "none";
+}
