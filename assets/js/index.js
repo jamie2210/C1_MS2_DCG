4b4917d2ad4f93d747f42e0ca767da7036fbd8a9
jamie2210 <jamieletts2210@gmail.com>
2023-03-13 13:32:55 +0000

    Feature: Add darkened background when model opens

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,19 +4,26 @@ let riddlerModal = document.querySelector('.riddler-modal');
 
 let button = document.getElementsByClassName('close');
 
+let batmanBackground = document.querySelector('.batman-background');
+
+let riddlerBackground = document.querySelector('.riddler-background');
+
 function batmanRules() {
     batmanModal.style.display = "block";
-
+    batmanBackground.style.display = "block";
 }
 
 function riddlerRules() {
     riddlerModal.style.display = "block";
+    riddlerBackground.style.display = "block";
 }
 
 function batmanClose() {
     batmanModal.style.display = "none";
+    batmanBackground.style.display = "none";
 }
 
 function riddlerClose() {
     riddlerModal.style.display = "none";
+    riddlerBackground.style.display = "none";
 }
